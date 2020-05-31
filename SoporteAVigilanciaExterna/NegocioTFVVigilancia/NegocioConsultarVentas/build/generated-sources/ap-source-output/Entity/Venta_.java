595fa1685354cc32a113ffd056871fc4fac266af
SantiagoChaparro <41376467+SantiagoChaparro@users.noreply.github.com>
2020-05-31 11:16:03 -0500

    correccion

--- SoporteAVigilanciaExterna/NegocioTFVVigilancia/NegocioConsultarVentas/build/generated-sources/ap-source-output/Entity/Venta_.java
+++ SoporteAVigilanciaExterna/NegocioTFVVigilancia/NegocioConsultarVentas/build/generated-sources/ap-source-output/Entity/Venta_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-29T15:27:55")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-31T11:12:56")
 @StaticMetamodel(Venta.class)
 public class Venta_ { 
 
